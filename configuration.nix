be2dbd8a51870c71530380ba8004967fe0665d1c
Charles <58385533+chyoung1@users.noreply.github.com>
2021-09-02 06:54:24 +0000

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,186 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.useOSProber = true;
+  networking.hostName = "nixos"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.interfaces.wlp1s0.useDHCP = true;
+  networking.networkmanager.enable = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "br-abnt2";
+   };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
+
+  
+
+  # Configure keymap in X11
+  services.xserver.layout = "br";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.charles = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   nixpkgs.config.allowUnfree = true;
+   environment.systemPackages = with pkgs; [
+     	vim 
+     	wget
+     	qutebrowser
+     	chromium
+     	i3-gaps
+	i3ipc-glib
+	i3-auto-layout
+	i3status
+	i3pystatus
+	i3blocks-gaps	
+	git
+	curl
+	nix-bash-completions
+	pavucontrol
+	python39Packages.pip
+	alacritty
+	rofi
+	polybar
+	networkmanager
+	dmenu
+	networkmanager_dmenu
+	networkmanagerapplet
+	neofetch
+	flameshot
+	arandr
+	picom
+	pywal
+	feh
+	imagemagick
+	python39
+	pythonFull
+	python39Packages.i3ipc
+	ranger
+	ueberzug
+	flatpak
+	spotify
+	lxappearance
+	solarc-gtk-theme
+	papirus-icon-theme
+	vscode-with-extensions
+	vscode-extensions.ms-python.python
+	vscode-extensions.vscodevim.vim
+	vscode-extensions.redhat.java
+	libsForQt5.kdeconnect-kde
+	gotop
+	htop
+	vimPlugins.coc-nvim	
+	vimPlugins.spacevim
+	xorg.xkill
+	pfetch
+	steam-run-native
+	steam
+	mpv
+	ncspot
+	cava
+	unzip
+	unrar
+	xfce.thunar
+	xfce.thunar-volman
+	xfce.thunar-archive-plugin
+	os-prober
+   ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+   programs.mtr.enable = true;
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
+
+nixpkgs.config.allowBroken = true;
+  # List services that you want to enable:
+   services.flatpak.enable = true;
+   xdg.portal.enable = true;
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Steam support
+   nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      nativeOnly = true;
+    };
+  };
+  programs.steam.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  	networking.firewall.allowedTCPPortRanges = [
+  	{ from = 1714; to = 1764; }
+	];
+
+  	networking.firewall.allowedUDPPortRanges = [
+  	{ from = 1714; to = 1764; }
+	];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
+
